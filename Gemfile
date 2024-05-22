41872ee11e49796de5eb6f2f09d4361fdd092dc7
Guilherme Ramos <gcsramos@id.uff.br>
2024-05-21 21:48:06 -0300

    added test gems

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ]
   gem "pry-rails"
   gem "rspec-rails"
+  gem 'shoulda-matchers'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,3 +63,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
