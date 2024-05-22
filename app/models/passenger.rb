279981fbfebe6eaa9c074298dc9c96e4ade9bba8
fxrrvz <lucastadeuferraz@gmail.com>
2024-05-22 20:32:44 -0300

    edited models passenger, vehicle and user

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
   belongs_to :user
+
+  scope :with_lift, ->() { Lift.where(passenger_id: id, status: :active) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  # fazer os validates
+  validates :iduff, presence: true
+  validates :name, presencece:true
+
+  scope :with_lift, ->() { Lift.where(user_id: id, status: :active) }
 end
