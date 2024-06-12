fd7b4f81ecd992dc7319b943574591a04d201558
Guilherme Ramos <gcsramos@id.uff.br>
2024-06-12 18:10:30 -0300

    adicionado endpoints da API de lifts com specs

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -15,50 +15,53 @@ class LiftsController < ApplicationController
 
   # POST /lifts or /lifts.json
   def create
-    @lift = Lift.new(lift_params)
-    respond_to do |format|
-      if @lift.save
-        format.html { redirect_to lift_url(@lift), notice: "Lift was successfully created." }
-        format.json { render :show, status: :created, location: @lift }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @lift.errors, status: :unprocessable_entity }
-      end
+    if lift_params[:driver_id].present?
+      @lift = Lift.create!(lift_params)
+
+    elsif lift_params[:passenger_id].present?
+      @lift = Lift.create!(lift_params.except(:dropoff_location, :pickup_location, :passenger_id))
+      LiftPassenger.create!(lift_params.merge(lift: @lift).except(:status))
     end
+
+    render json: @lift, status: :created
+  rescue
+    render json: { error: 'failed to create lift' }, status: :bad_request
   end
 
   # PATCH/PUT /lifts/1 or /lifts/1.json
   def update
-    respond_to do |format|
-      if @lift.update(lift_params)
-        format.html { redirect_to lift_url(@lift), notice: "Lift was successfully updated." }
-        format.json { render :show, status: :ok, location: @lift }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @lift.errors, status: :unprocessable_entity }
-      end
+    if lift_params[:driver_id].present?
+      @lift.driver_id = lift_params[:driver_id]
+      @lift.start_location = lift_params[:start_location]
+      @lift.end_location = lift_params[:end_location]
+
+    elsif lift_params[:passenger_id].present?
+      LiftPassenger.create!(lift_params.merge(lift: @lift).except(:status))
+
     end
+    @lift.save
+
+    render json: @lift, status: :ok
+  rescue
+    render json: { error: 'error, try again' }, status: :bad_request
   end
 
   # DELETE /lifts/1 or /lifts/1.json
   def destroy
     @lift.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to lifts_url, notice: "Lift was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    render json: "destroyed", status: :ok
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lift
-      @lift = Lift.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_lift
+    @lift = Lift.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def lift_params
-      # {lift: {driver_id: int, passenger_id: int, status: int, start_location: string, end_location: string}}
-      params.permit(:driver_id, :passenger_id, :status, :start_location, :end_location)
-    end
+  # Only allow a list of trusted parameters through.
+  def lift_params
+    # {lift: {driver_id: int, passenger_id: int, status: int, start_location: string, end_location: string}}
+    params.require(:lift).permit(:id, :driver_id, :passenger_id, :status, :start_location, :end_location, :pickup_location, :dropoff_location)
+  end
 end
